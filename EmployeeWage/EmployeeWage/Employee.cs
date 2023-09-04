d058c9607cc0d840eff763893836ba6e2c0d0816
19AmitKumar <ak9978292@gmail.com>
2023-09-04 09:24:15 +0530

    [Amit] Add:Calculating Employee daily Wage

--- EmployeeWage/EmployeeWage/Employee.cs
+++ EmployeeWage/EmployeeWage/Employee.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,18 +9,28 @@ namespace EmployeeWage
 {
     internal class Employee
     {
+        const int wagePerHour = 20, isPresent = 0, isFullTime = 1;
+        int fullDayHour = 0;
+
+
         public void Attendance()
         {
             Random random = new Random();
             int empCheck=random.Next(0, 2);
-            if (empCheck == 0)
+            if (empCheck == isFullTime)
             {
                 Console.WriteLine("Employee is Present.");
+                fullDayHour = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent.");
+                fullDayHour = 0;
             }
+            int empWage = fullDayHour * wagePerHour;
+            Console.WriteLine("Daily wage is: "+empWage);
+
         }
+
     }
 }
