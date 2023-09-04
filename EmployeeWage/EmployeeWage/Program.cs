1dccdc742bcf193a280c1aae9a54c32f18343ab8
19AmitKumar <ak9978292@gmail.com>
2023-09-04 08:59:42 +0530

    [Amit] Add: Employee present or absent

---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Attendance();
+
+
+            Console.ReadLine();
+        }
+        
+    }
+}
