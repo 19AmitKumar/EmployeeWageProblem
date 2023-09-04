ec3cc7b7f1deb6340772fb4f2426ae167c446bbb
19AmitKumar <ak9978292@gmail.com>
2023-09-04 14:16:55 +0530

    [Amit] Add:Calculate wage for a month

--- EmployeeWage/EmployeeWage/Employee.cs
+++ EmployeeWage/EmployeeWage/Employee.cs
@@ -9,33 +9,38 @@ namespace EmployeeWage
 {
     internal class Employee
     {
-        const int wagePerHour = 20, isPresent = 0,isPartTime=1, isFullTime = 2;
-        int fullDayHour = 0;
+        const int wagePerHour = 20, isPresent = 0, isPartTime = 1, isFullTime = 2;
+        const int noOfWorkingDays = 2;
+        int fullDayHour = 0, totalEmpWage = 0;
 
 
         public void Attendance()
         {
             Random random = new Random();
-            int empCheck=random.Next(0, 3);
-             switch (empCheck)
-            {
-                case isFullTime:
-                Console.WriteLine("Employee is Present.");
-                fullDayHour = 8;
-                    break;
-                case isPartTime:
-                Console.WriteLine("Employee is Present.");
-                fullDayHour = 4;
-                    break;
-                default:
-                Console.WriteLine("Employee is Absent.");
-                fullDayHour = 0;
-                   break;
+            int empCheck = random.Next(0, 3);
+            for (int day = 0; day < noOfWorkingDays; day++) {
+                switch (empCheck)
+                {
+                    case isFullTime:
+                        Console.WriteLine("Employee is Present.");
+                        fullDayHour = 8;
+                        break;
+                    case isPartTime:
+                        Console.WriteLine("Employee is Present.");
+                        fullDayHour = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent.");
+                        fullDayHour = 0;
+                        break;
+                }
+                int empWage = fullDayHour * wagePerHour;
+                totalEmpWage += empWage;
+                Console.WriteLine("Daily wage is: " + empWage);
             }
-            int empWage = fullDayHour * wagePerHour;
-            Console.WriteLine("Daily wage is: "+empWage);
+            Console.WriteLine("Total Emp wage: " + totalEmpWage);
 
-        }
-
+    } 
+    
     }
 }
